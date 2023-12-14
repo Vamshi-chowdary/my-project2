b943fb0f240c965b6615890c3dfc6931dd3cdb78
vamshi <narkhudavamshichowdary@gmail.com>
2023-12-14 14:45:46 +0530

    done

---
+++ my-project2/src/main/java/in/vamshitech/App.java
@@ -0,0 +1,13 @@
+package in.vamshitech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
